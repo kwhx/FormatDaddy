ae05cf27e61bc14eb707ccb8ea25783f37d513b6
0xrootAnon <khwahish.dev@gmail.com>
2025-10-10 15:49:46 +0530

    integrated speed-insights

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,12 @@
-// pages/_app.js
-import "../styles/globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Component {...pageProps} />
+      <SpeedInsights />  
+    </>
+  );
 }
+
+export default MyApp;
