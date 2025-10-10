f6e9b22a2d9eef2134b123d74a64d996718c73a0
0xrootAnon <khwahish.dev@gmail.com>
2025-10-10 16:52:29 +0530

    removed trash

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,12 +1,14 @@
 // src/pages/_app.js
 import "../styles/globals.css";            // <- MUST import global CSS here
 import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next"
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
       <SpeedInsights />
+      <Analytics />
     </>
   );
 }
